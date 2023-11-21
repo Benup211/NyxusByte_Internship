d2d14933f9a398bb138679114fb72e7687c0a426
benp ghimire <benp211@gmail.com>
2023-11-21 20:53:19 +0545

    added to continue

--- Task/task_2.py
+++ Task/task_2.py
@@ -4,17 +4,27 @@ import random
 try:
     guessed=False
     random_gen_number=random.randint(1,100)
-    for i in range(3):
-        user_input_number=int(input("Guess any number betn 1-100:"))
-        if user_input_number==random_gen_number:
-            guessed=True
-            break
-        elif user_input_number<=random_gen_number:
-            print(f"Try guessing higher number than {user_input_number}")
-        elif user_input_number<0:
-            print("Enter positive value")
+    while(True):
+        to_continue=False
+        for i in range(3):
+            user_input_number=int(input("Guess any number betn 1-100:"))
+            if user_input_number==random_gen_number:
+                guessed=True
+                break
+            elif user_input_number<=random_gen_number:
+                print(f"Try guessing higher number than {user_input_number}")
+            elif user_input_number<0:
+                print("Enter positive value")
+            else:
+                print(f"Try guessing lower number than {user_input_number}")
+        if(guessed):
+            print("Sucessfully guessed number:",random_gen_number)
         else:
-            print(f"Try guessing lower number than {user_input_number}")
-            
+            continue_guess=input("Do you want to continue[y/n]:").lower()
+            if continue_guess=='y':
+                to_continue=True
+        if not to_continue:
+            print("Too bad! Number is",random_gen_number)
+            break
 except:
     print("User input should be number only")
