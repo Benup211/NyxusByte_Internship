ef82343254d0415f37874bd7b1a9653cba84122f
benp ghimire <benp211@gmail.com>
2023-11-22 20:24:36 +0545

    Task 3 complete

--- day2_task1/ques3.py
+++ day2_task1/ques3.py
@@ -1,6 +1,7 @@
 class library:
     def __init__(self):
         print("Library management instance created")
+        self.book_inventory()
         self.help_message()
     def help_message(self):
         print(f"{'Library Book Management Command':#^100}")
@@ -9,4 +10,38 @@ class library:
         print(f"{'display->display the current books in the inventory':-^100}")
         print(f"{'exit->exit the program':-^100}")
     def book_inventory(self):
-        
+        self._books=list()
+        print("Empty book list created")
+    def book_addition(self,book_name):
+        self._books.append(book_name)
+    def book_removal(self,book_name):
+        if book_name in self._books:
+            self._books.remove(book_name)
+        else:
+            print("Book is not found in inventory")
+    def inventory_display(self):
+        if len(self._books)>0:
+            print(f"Book inventory is {self._books}")
+        else:
+            print("Book Inventory is empty")
+Nyxus_library=library()
+while(1):
+    command=input("Enter your command [add,remove,display,exit]:")
+    match command:
+        case 'add':
+            add_book=input("Enter book name to add in the inventory:")
+            Nyxus_library.book_addition(add_book)
+        case 'remove':
+            remove_book=input("Enter book name to remove from inventory:")
+            Nyxus_library.book_removal(remove_book)
+        case 'display':
+            Nyxus_library.inventory_display()
+        case 'exit':
+            choice=input("Are you sure you want to exit[yes/no]:").lower()
+            if choice=='yes' or choice=='y':
+                del Nyxus_library
+                break
+            else:
+                print("Continuing....")
+        case default:
+            print("Enter correct command [add,remove,display,exit]")
